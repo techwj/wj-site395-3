997d049a9c94617103bf9d8e6efe50353b06b412
techwj <weijutech@outlook.com>
2026-05-21 16:27:07 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+<!-- app.vue -->
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+      async: true,
+      crossorigin: 'anonymous',
+      'data-ad-client': 'ca-pub-2554404510173364'  // 你的发布商ID
+    }
+  ]
+})
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
